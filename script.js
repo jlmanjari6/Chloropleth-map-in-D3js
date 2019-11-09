4c671783d576ff4181de34e8e3f245d74db896cb
Manjari <lk659879@dal.ca>
2019-11-09 16:28:07 -0400

    hover over province and details added

--- script.js
+++ script.js
@@ -5,29 +5,22 @@ var centered;
 var svg = d3.select("svg");
 var g = d3.select("g");
 
-var projection = d3.geo.albersUsa()
+var projection = d3.geoAlbersUsa()
     .scale(1070)
     .translate([WIDTH / 2, HEIGHT / 2]);
 
-var path = d3.geo.path()
+var path = d3.geoPath()
     .projection(projection);
 
-// Append Div for tooltip to SVG
-var div = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-d3.json("/canada-province.json", function (error, cp) {
-    if (error) throw error;
-
+d3.json("/canada-province.json").then(function (cp) {
     // Add the labels
-    d3.selectAll('path') 
+    d3.selectAll('path')
         .each(function (d) {
+            currPathEle = d3.select(this)["_groups"][0][0];
             svg.append("text")
-                .attr("x", findCentroid(d3.select(this)[0][0])[0])
-                .attr("y", findCentroid(d3.select(this)[0][0])[1])
-                .text(d3.select(this)[0][0].id)
+                .attr("x", findCentroid(currPathEle)[0])
+                .attr("y", findCentroid(currPathEle)[1])
+                .text(currPathEle.id)
                 .attr("text-anchor", "middle")
                 .attr("alignment-baseline", "central")
                 .style("font-size", 11)
@@ -38,7 +31,9 @@ d3.json("/canada-province.json", function (error, cp) {
     d3.selectAll("path")
         .data(cp.features)
         .on("mouseover", function (d) {
+            currPathEle = d3.select(this)["_groups"][0][0];
             d3.select(this).style('stroke', 'black');
+            getTableData(currPathEle.id);
         })
 
         // fade out tooltip on mouse out               
@@ -48,13 +43,72 @@ d3.json("/canada-province.json", function (error, cp) {
         .on("click", clicked);
 });
 
-// $(document).ready( function() {
-//     $('#CABC').css('fill', 'red')   
-// });
+$(document).ready(function () {
+
+});
 
 function findCentroid(element) {
-    var bbox = element.getBBox();
-    return [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
+    bbox = element.getBBox();
+    centroid = [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
+    return centroid;
+}
+
+function getTableData(id) {
+    var provinceSelected = ""
+    if (id == 'CA.NB') {
+        provinceSelected = "New Brunswick";
+    }
+    else if (id == 'CA.BC') {
+        provinceSelected = "British Columbia";
+    }
+    d3.csv("processed_data.csv").then(function (data) {
+        numListings = 0;
+        numEntireHome = 0;
+        numPrivateRoom = 0;
+        numHotelRoom = 0;
+        numSharedRoom = 0;
+        numNeighborhoods = 0;
+
+        numRows = data.length;
+
+        //num of listings in province
+        for (i = 0; i < numRows; i++) {
+            if (data[i]['province'] == provinceSelected) {
+                numListings++;
+            }
+        }
+
+        // num of listings by room type
+        classes = d3.map(data, function (d) { return d['room_type']; }).keys();
+        for (i = 0; i < data.length; i++) {
+            if (data[i]['province'] == provinceSelected && data[i]['room_type'] == classes[0]) {
+                numEntireHome++;
+            }
+            else if (data[i]['province'] == provinceSelected && data[i]['room_type'] == classes[1]) {
+                numPrivateRoom++;
+            }
+            else if (data[i]['province'] == provinceSelected && data[i]['room_type'] == classes[2]) {
+                numHotelRoom++;
+            }
+            else if (data[i]['province'] == provinceSelected && data[i]['room_type'] == classes[3]) {
+                numSharedRoom++;
+            }
+        }
+
+        // num of neighbourhoods
+        numNeighborhoods = (d3.map(data, function (d) {
+            if (d['province'] == provinceSelected) {
+                return d['neighbourhood'];
+            }
+        }).keys()).length;
+
+        document.getElementById("numListings").innerHTML = numListings;
+        document.getElementById("numEntireHome").innerHTML = numEntireHome;
+        document.getElementById("numPrivateRoom").innerHTML = numPrivateRoom;
+        document.getElementById("numHotelRoom").innerHTML = numHotelRoom;
+        document.getElementById("numSharedRoom").innerHTML = numSharedRoom;
+        document.getElementById("numNeighborhoods").innerHTML = numNeighborhoods;
+    });
 }
 
 function clicked(d, i) {
